8eab4d4ba55a90a197338f401f5eed4f5217fcce
Duy Le <lehongduy.bk@gmail.com>
2022-04-28 19:09:47 +0700

    update firebase cloud FireStore

--- lib/GetUserName.dart
+++ lib/GetUserName.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class GetUserName extends StatelessWidget {
   final String documentId;
@@ -12,26 +11,30 @@ class GetUserName extends StatelessWidget {
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-    return FutureBuilder<DocumentSnapshot>(
-      future: users.doc(documentId).get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<DocumentSnapshot>(
+          future: users.doc(documentId).get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
 
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return Text("Document does not exist");
+            }
 
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return Text("Document does not exist");
-        }
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+              return Text("Full Name: ${data['full_name']} ${data['company']}");
+            }
 
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-          return Text("Full Name: ${data['full_name']} ${data['last_name']}");
-        }
-
-        return Text("loading");
-      },
+            return Text("loading");
+          },
+        ),
+      ),
     );
   }
 }
